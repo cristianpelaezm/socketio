a1b6585ceb816dfc9c39c77af6cd084b9b7352f7
Your Name <cristianpelaezm@gmail.com>
2020-03-25 15:12:52 -0500

    add segurity... error

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
 const mongo = require('mongodb').MongoClient;
 const client = require('socket.io').listen(4000).sockets;
+const socketioJwt = require('socketio-jwt');
+
+
 
 // Connect to mongo
-mongo.connect('mongodb://127.0.0.1/component', function(err, db){
-    if(err){
+mongo.connect('mongodb://127.0.0.1/component', function (err, db) {
+    if (err) {
         throw err;
     }
 
     console.log('MongoDB connected...');
 
     // Connect to Socket.io
-    client.on('connection', function(socket){
+    client.on('connection', socketioJwt.authorize({
+        secret: '123',
+        timeout: 15000 //15 segundos para enviar el mensaje de autenticación
+    })).on('authenticated', function (socket) {
         let chat = db.collection('componentes');
- 
+
         // Handle input events
-        socket.on('input', function(data){
+        socket.on('input', function (data) {
+            console.log(data);
             let name = data.name;
             let message = data.message;
 
             // Check for name and message
-             
-                // Insert message
-                chat.insert({name: name, message: message}, function(){
-                    client.emit('output', [data]);
 
-                    // Send status object
-                    sendStatus({
-                        message: 'Message sent',
-                        clear: true
-                    });
-                });
-            
+            // Insert message
+            chat.insert({ name: name, message: message }, function () {
+                client.emit('output', [data]); 
+            });
+
         });
 
-       
+        console.log('hello! ' + socket.decoded_token.name);
     });
+
+
 });
